4a4cb98b25e51c57f61c60c2d05b1f8f15849653
Trend20 <enockomondi305@gmail.com>
2024-09-05 00:00:16 +0300

    adding a test router instance

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Golang Shopper API")
+
+	//	initialize a gin router instance
+	router := gin.Default()
+
+	//	sample test route
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World",
+		})
+	})
+
+	// APPLICATION ROUTES HERE
+
+	//listen to the port
+	router.Run(":5000")
 }
