8c4c60bb1bb508134f14a955285cfee3f269b3a2
Trend20 <enockomondi305@gmail.com>
2024-09-06 23:08:15 +0300

    adding update product model

--- models/product.go
+++ models/product.go
@@ -9,3 +9,21 @@ type Product struct {
 	Images      string `json:"images"`
 	Category    string `json:"category"`
 }
+
+type CreateProductInput struct {
+	Name        string `json:"name" binding:"required"`
+	Description string `json:"description" binding:"required"`
+	Price       uint   `json:"price" binding:"required"`
+	Stock       uint   `json:"stock" binding:"required"`
+	Images      string `json:"images" binding:"required"`
+	Category    string `json:"category" binding:"required"`
+}
+
+type UpdateProductInput struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Price       uint   `json:"price"`
+	Stock       uint   `json:"stock"`
+	Images      string `json:"images"`
+	Category    string `json:"category"`
+}
