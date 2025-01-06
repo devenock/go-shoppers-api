e3291e7472a840e62283763fe667dc38225993c1
Trend20 <enockomondi305@gmail.com>
2025-01-06 19:24:23 +0300

    adding the product repository

---
+++ internal/modules/products/repositories/product_repository.go
@@ -0,0 +1,51 @@
+package repositories
+
+import (
+	"github.com/Trend20/go-shoppers-api/internal/modules/products/models"
+	"github.com/Trend20/go-shoppers-api/pkg/db"
+)
+
+// ProductRepository repository structure
+type ProductRepository struct{}
+
+// GetAll products
+func (r *ProductRepository) GetAll() ([]models.Product, error) {
+	var products []models.Product
+	if err := db.DB.Find(&products).Error; err != nil {
+		return nil, err
+	}
+	return products, nil
+}
+
+// GetByID
+func (r *ProductRepository) GetByID(id int) (*models.Product, error) {
+	var product models.Product
+	if err := db.DB.First(&product, id).Error; err != nil {
+		return nil, err
+	}
+	return &product, nil
+}
+
+// Create Product
+func (r *ProductRepository) Create(product *models.Product) error {
+	if err := db.DB.Create(product).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+// Update Product
+func (r *ProductRepository) Update(product *models.Product) (*models.Product, error) {
+	if err := db.DB.Save(product).Error; err != nil {
+		return nil, err
+	}
+	return product, nil
+}
+
+// Delete Product
+func (r *ProductRepository) Delete(id int) error {
+	if err := db.DB.Delete(&models.Product{}, id).Error; err != nil {
+		return err
+	}
+	return nil
+}
