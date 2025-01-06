8e04ba52bdfd96b78ed307674905865ec6d46610
Trend20 <enockomondi305@gmail.com>
2025-01-06 21:51:28 +0300

    adding the product service

--- internal/modules/products/services/product_service.go
+++ internal/modules/products/services/product_service.go
@@ -13,27 +13,22 @@ func NewProductService(repo *repositories.ProductRepository) *ProductService {
 	return &ProductService{repo: repo}
 }
 
-// get all products
 func (s *ProductService) GetAllProducts() ([]models.Product, error) {
 	return s.repo.GetAll()
 }
 
-// get product by ID
 func (s *ProductService) GetProductById(id uint) (*models.Product, error) {
 	return s.repo.GetByID(id)
 }
 
-// create a new product
 func (s *ProductService) CreateProduct(product *models.Product) error {
 	return s.repo.Create(product)
 }
 
-// update product
 func (s *ProductService) UpdateProduct(product *models.Product) error {
 	return s.repo.Update(product)
 }
 
-// delete product
 func (s *ProductService) DeleteProduct(id uint) error {
 	return s.repo.Delete(id)
 }
