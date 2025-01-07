b6d0309bd3014a174744e3950e165e9fe4e51340
Trend20 <enockomondi305@gmail.com>
2025-01-07 13:00:27 +0300

    routing bug fixes

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -1,25 +1,31 @@
 package routes
 
 import (
-	"github.com/Trend20/go-shoppers-api/internal/modules/products/controllers"
-	"github.com/Trend20/go-shoppers-api/internal/modules/products/repositories"
-	"github.com/Trend20/go-shoppers-api/internal/modules/products/routes"
-	"github.com/Trend20/go-shoppers-api/internal/modules/products/services"
+	productControllers "github.com/Trend20/go-shoppers-api/internal/modules/products/controllers"
+	productRepositories "github.com/Trend20/go-shoppers-api/internal/modules/products/repositories"
+	productRoutes "github.com/Trend20/go-shoppers-api/internal/modules/products/routes"
+	productServices "github.com/Trend20/go-shoppers-api/internal/modules/products/services"
+
+	userControllers "github.com/Trend20/go-shoppers-api/internal/modules/users/controllers"
+	userRepositories "github.com/Trend20/go-shoppers-api/internal/modules/users/repositories"
+	userRoutes "github.com/Trend20/go-shoppers-api/internal/modules/users/routes"
+	userServices "github.com/Trend20/go-shoppers-api/internal/modules/users/services"
+
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterAllRoutes(router *gin.Engine) {
-	api := router.Group("/routes/v1")
+	api := router.Group("/api/v1")
 
 	// Product module setup
-	productRepo := &repositories.ProductRepository{}
-	productService := services.NewProductService(productRepo)
-	productController := controllers.NewProductController(productService)
-	routes.RegisterProductRoutes(api, productController)
+	productRepo := &productRepositories.ProductRepository{}
+	productService := productServices.NewProductService(productRepo)
+	productController := productControllers.NewProductController(productService)
+	productRoutes.RegisterProductRoutes(api, productController)
 
 	// User Module setup
-	//userRepo := &repositories.UserRepository{}
-	//userService := services.NewUserService{UserRepo}
-	//userController := controllers.NewUserController{userService}
-	//routes.RegisterUserRoutes(api, userController)
+	userRepo := &userRepositories.UserRepository{}
+	userService := userServices.NewUserService(userRepo)
+	userController := userControllers.NewUserController(userService)
+	userRoutes.RegisterUserRoutes(api, userController)
 }
